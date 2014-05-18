1daa811be6c0278a49efcdcb29de82b1746f406c
Luiz Felipe <lfschleder@gmail.com>
2014-05-18 16:25:39 -0300

    user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+   
+    if @user.save  # redireciona para a url que exibe o user
+      redirect_to @user, notice: "Thank you for signing up for Ribbit!"
+    else           # ou redirecina para /users/new e exibe os erros. O formulario eh populado com os valores anteriores.
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+  
 end
