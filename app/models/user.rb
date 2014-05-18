102d9e41814b8aae2e068af5e2accc8bf4c7ef14
Luiz Felipe <lfschleder@gmail.com>
2014-05-18 18:49:06 -0300

    created user relationships infrastructure

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   attr_accessible :avatar_url, :email, :name, :username, :password, :password_confirmation
+
   has_many :ribbits
+  has_many :follower_relationships, classname: "Relationship", foreign_key: "followed_id"
+  has_many :followed_relationships, classname: "Relationship", foreign_key: "follower_id"
 
   has_secure_password  
   before_validation :prep_email
@@ -9,6 +12,15 @@ class User < ActiveRecord::Base
   validates :name, presence: true
   validates :username, uniqueness: true, presence: true
   validates :email, uniqueness: true, presence: true, format: { with: /^[\w.+-]+@([\w]+.)+\w+$/ }
+
+  def following? user
+    self.followeds.include? user
+  end
+   
+  def follow user
+      Relationship.create follower_id: self.id, followed_id: user.id
+  end
+  
   
   private 
 
