70d69f2e342cce91f13a38466a4ee9ffa7be3492
Luiz Felipe <lfschleder@gmail.com>
2014-05-18 21:12:43 -0300

    follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
   attr_accessible :avatar_url, :email, :name, :username, :password, :password_confirmation
 
   has_many :ribbits
-  has_many :follower_relationships, classname: "Relationship", foreign_key: "followed_id"
-  has_many :followed_relationships, classname: "Relationship", foreign_key: "follower_id"
+  has_many :follower_relationships, class_name: "Relationship", foreign_key: "followed_id"
+  has_many :followed_relationships, class_name: "Relationship", foreign_key: "follower_id"
 
   has_secure_password  
   before_validation :prep_email
@@ -14,14 +14,14 @@ class User < ActiveRecord::Base
   validates :email, uniqueness: true, presence: true, format: { with: /^[\w.+-]+@([\w]+.)+\w+$/ }
 
   def following? user
-    self.followeds.include? user
+    self.followed_relationships.include? user
   end
    
   def follow user
       Relationship.create follower_id: self.id, followed_id: user.id
   end
   
-  
+
   private 
 
   def prep_email
